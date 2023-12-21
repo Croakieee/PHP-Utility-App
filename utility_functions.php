7316af6883e679a4b2523a8074b13662398960ad
croakie_ <71844595+Croakieee@users.noreply.github.com>
2023-12-22 01:16:19 +0400

    Create utility_functions.php

---
+++ utility_functions.php
@@ -0,0 +1,63 @@
+<?php
+
+function GenerateRandomPassword($length) {
+    // Implementation for generating a random password
+}
+
+function CalculateAge($birthdate) {
+    // Implementation for calculating age
+}
+
+function FormatCurrency($amount, $currencyCode) {
+    // Implementation for formatting currency
+}
+
+function SendEmail($to, $subject, $message) {
+    // Implementation for sending an email
+}
+
+function EncryptString($stringToEncrypt) {
+    // Implementation for encrypting a string
+}
+
+function DecryptString($encryptedString) {
+    // Implementation for decrypting a string
+}
+
+function GenerateThumbnail($sourceImagePath, $thumbnailImagePath, $width, $height) {
+    // Implementation for generating a thumbnail
+}
+
+function IsWeekend($dateToCheck) {
+    // Implementation for checking if a date is a weekend
+}
+
+function ValidateEmail($emailToValidate) {
+    // Implementation for validating an email address
+}
+
+function GenerateQRCode($textToEncode, $savePath) {
+    // Implementation for generating a QR code
+}
+
+function GenerateUUID() {
+    // Implementation for generating a UUID
+}
+
+function CompressImage($sourceImagePath, $compressedImagePath) {
+    // Implementation for compressing an image
+}
+
+function ReverseString($inputString) {
+    // Implementation for reversing a string
+}
+
+function ConvertToUppercase($inputString) {
+    // Implementation for converting a string to uppercase
+}
+
+function GetFileExtension($filePath) {
+    // Implementation for retrieving the file extension
+}
+
+?>
